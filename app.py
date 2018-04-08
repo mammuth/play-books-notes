5de0ba4fe6fc7a88e5438637a3601111182678e5
Max Muth <kontakt@maxi-muth.de>
2018-04-08 22:21:51 +0200

    Init

---
+++ app.py
@@ -0,0 +1,148 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+import flask
+import requests
+
+import google.oauth2.credentials
+import google_auth_oauthlib.flow
+
+from flask import render_template, redirect, url_for
+
+import notes
+
+
+app = flask.Flask(__name__)
+app.secret_key = 'REPLACE ME - this value is here as a placeholder.'  # ToDo secret key
+
+ROOT_PATH = os.path.join(app.root_path, '')
+
+CLIENT_SECRETS_FILE = f"{ROOT_PATH}client_secret.json"  # Download that file from the Google API Console
+
+OAUTH_SCOPES = [
+    'https://www.googleapis.com/auth/drive.readonly',
+]
+API_SERVICE_NAME = 'drive'
+API_VERSION = 'v3'
+
+
+@app.route('/')
+def index():
+    return redirect(url_for('list_notes'))
+
+
+# ToDo
+# - Use same view but behave according to accepted content-type header
+# - Display total number of quotes
+@app.route('/notes')
+def list_notes():
+    if 'credentials' not in flask.session:
+        return flask.redirect('authorize')
+
+    context = {
+        'notes': notes.get_notes(),
+    }
+    return render_template('notes.html', **context)
+
+
+# ToDo
+# - Don't only parse and store quote text, but also chapter title and page number
+@app.route('/notes-api')
+def list_notes_api():
+    if 'credentials' not in flask.session:
+        return flask.redirect('authorize')
+
+    return flask.jsonify(**notes.get_notes())
+
+
+@app.route('/authorize')
+def authorize():
+    # Create flow instance to manage the OAuth 2.0 Authorization Grant Flow steps.
+    flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+        CLIENT_SECRETS_FILE, scopes=OAUTH_SCOPES)
+
+    flow.redirect_uri = flask.url_for('oauth2callback', _external=True)
+
+    authorization_url, state = flow.authorization_url(
+        # Enable offline access so that you can refresh an access token without
+        # re-prompting the user for permission. Recommended for web server apps.
+        access_type='offline',
+        # Enable incremental authorization. Recommended as a best practice.
+        include_granted_scopes='true')
+
+    # Store the state so the callback can verify the auth server response.
+    flask.session['state'] = state
+
+    return flask.redirect(authorization_url)
+
+
+@app.route('/oauth2callback')
+def oauth2callback():
+    # Specify the state when creating the flow in the callback so that it can
+    # verified in the authorization server response.
+    state = flask.session['state']
+
+    flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+        CLIENT_SECRETS_FILE, scopes=OAUTH_SCOPES, state=state)
+    flow.redirect_uri = flask.url_for('oauth2callback', _external=True)
+
+    # Use the authorization server's response to fetch the OAuth 2.0 tokens.
+    authorization_response = flask.request.url
+    flow.fetch_token(authorization_response=authorization_response)
+
+    # Store credentials in the session.
+    # ACTION ITEM: In a production app, you likely want to save these
+    #              credentials in a persistent database instead.
+    credentials = flow.credentials
+    flask.session['credentials'] = credentials_to_dict(credentials)
+
+    return flask.redirect(flask.url_for('list_notes'))
+
+
+@app.route('/revoke')
+def revoke():
+    if 'credentials' not in flask.session:
+        return ('You need to <a href="/authorize">authorize</a> before ' +
+                'testing the code to revoke credentials.')
+
+    credentials = google.oauth2.credentials.Credentials(
+        **flask.session['credentials'])
+
+    revoke = requests.post('https://accounts.google.com/o/oauth2/revoke',
+                           params={'token': credentials.token},
+                           headers={'content-type': 'application/x-www-form-urlencoded'})
+
+    status_code = getattr(revoke, 'status_code')
+    if status_code == 200:
+        return 'Credentials successfully revoked.'
+    else:
+        return 'An error occurred.'
+
+
+@app.route('/clear')
+def clear_credentials():
+    if 'credentials' in flask.session:
+        del flask.session['credentials']
+    return 'Credentials have been cleared.<br><br>'
+
+
+def credentials_to_dict(credentials):
+    return {'token': credentials.token,
+            'refresh_token': credentials.refresh_token,
+            'token_uri': credentials.token_uri,
+            'client_id': credentials.client_id,
+            'client_secret': credentials.client_secret,
+            'scopes': credentials.scopes}
+
+
+if __name__ == '__main__':
+    # When running locally, disable OAuthlib's HTTPs verification.
+    # ACTION ITEM for developers:
+    #     When running in production *do not* leave this option enabled.
+    # Allow OAUTH to work without TLS for development
+    os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+
+    # Specify a hostname and port that are set as a valid redirect URI
+    # for your API project in the Google API Console.
+    app.run('127.0.0.1', 8080, debug=True)
---
+++ notes.py
@@ -0,0 +1,85 @@
+# ToDo
+# - Multiuser setup (use session key or something auth-related as new roots in NOTE_STORE. Grab that in get_notes()
+
+from typing import List
+
+import flask
+import io
+from bs4 import BeautifulSoup
+
+import google.oauth2.credentials
+import googleapiclient.discovery
+from googleapiclient.http import MediaIoBaseDownload
+
+from app import API_SERVICE_NAME, API_VERSION
+
+
+NOTE_STORE = {}
+
+
+def update_notes():
+    def extract_notes_from_html(html: str) -> List[str]:
+        soup = BeautifulSoup(html, 'html.parser')
+        note_tables = soup.select('table table')
+        quotes = []
+        for note in note_tables:
+            quote_element = note.select('td:nth-of-type(2) span')[0]
+            text = quote_element.contents[0]
+            quotes.append(text)
+        return quotes
+
+    credentials = google.oauth2.credentials.Credentials(
+        **flask.session['credentials'])
+
+    drive_service = googleapiclient.discovery.build(
+        API_SERVICE_NAME, API_VERSION, credentials=credentials)
+
+    # Get Notes Folder ID
+    notes_folder_search = drive_service.files().list(
+        q="name='Play Books Notes'"
+    ).execute()
+
+    if notes_folder_search:
+        files = notes_folder_search.get('files')
+        if len(files) > 0:
+            notes_folder_id = files[0].get('id')
+
+    # Get Notes IDs
+    notes = drive_service.files().list(
+        q=f"'{notes_folder_id}' in parents"
+    ).execute()
+
+    # for note in notes.get('files'):
+    for note in notes.get('files'):
+        note_id = note.get('id')
+        book_name = note.get('name')
+        book_name = book_name.replace('Notes from "', '').replace('Notizen aus "', '')[:-1]
+
+        # Download notes as HTML
+        note_file = drive_service.files().export_media(fileId=note_id,
+                                                       mimeType='text/html')
+        fh = io.BytesIO()
+        downloader = MediaIoBaseDownload(fh, note_file)
+        done = False
+        while done is False:
+            status, done = downloader.next_chunk()
+            print(f"Download {book_name} {int(status.progress()*100)}%.")
+
+        # Extract note text from raw html
+        raw_html = fh.getvalue().decode("utf-8")
+        final_notes = extract_notes_from_html(raw_html)
+
+        # Add to note store
+        NOTE_STORE[note_id] = {
+            'id': note_id,
+            'book_name': book_name,
+            'notes': final_notes,
+            'note_count': len(final_notes),
+        }
+
+
+def get_notes(user=None):
+    if NOTE_STORE:
+        return NOTE_STORE
+    update_notes()
+    return NOTE_STORE
